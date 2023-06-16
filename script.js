efc275f2937e5f6c6c02f843fb553182129418e4
sujal5g <121445004+sujal5g@users.noreply.github.com>
2023-06-16 16:57:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-let list=document.getElementById("lists");
-let display=document.getElementById("display");
-let btn=document.getElementById("Task");
-let message=document.getElementById('message');
-let newel;
-let flag=false;
-btn.addEventListener('click',()=>{
-    if(display.value==""){
-        message.innerHTML="Enter New Task";
+const item = document.querySelector("#display")
+const toDo = document.querySelector("#lists");
+
+           
+const newtask = () => {
+    if(item.value==""){
+        message.innerHTML="Enter New task";
     }else{
-        message.innerHTML="";
-        newel=document.createElement('li');
-        list.appendChild(newel);
-        newel.innerHTML=`${display.value} &#160 <svg onclick="myfuction()" class="closebtn
-        " xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 60 30">
-        <path d="M 6.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"></path>
-        </svg>`;
-        display.value="";
+        const newel = document.createElement("li");
+    newel.innerHTML = `
+         ${item.value}
+        <i class="fas fa-times"></i>
+    `;
+    newel.querySelector("i").addEventListener(
+        "click",
+        function() {
+            newel.remove()
+        }
+    )
+    message.innerHTML="";
+    item.value="";
+    toDo.appendChild(newel);
     }
-})
-function myfuction(){
-    list.removeChild(list.firstElementChild);
+    
 }
+
 
 
 
